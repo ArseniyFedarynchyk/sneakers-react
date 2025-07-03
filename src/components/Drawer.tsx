0fe96bdf26c4302241b1f26463a584102b27029c
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 15:26:50 +0200

    Create checkout template in drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -8,6 +8,19 @@ export default function Drawer() {
       <div className="fixed top-0 right-0 z-20 bg-white w-96 h-full p-8">
         <DrawerHeader />
         <CartItemList />
+
+        <div className="flex justify-between mb-6 mt-7">
+          <p>Sum:</p>
+          <div className="flex-1"></div>
+          <p>120 USD</p>
+        </div>
+
+        <button
+          disabled
+          className="mt-4 transition bg-lime-500 w-full rounded-xl cursor-pointer py-3 disabled:bg-slate-400 text-white hover:bg-lime-600 active:bg-lime-700"
+        >
+          Checkout
+        </button>
       </div>
     </>
   );
