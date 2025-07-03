3d3134a30fbe787dc56de5eb61f635a33f164996
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 15:26:27 +0200

    Use cart item list in drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,3 +1,4 @@
+import CartItemList from "./CartItemList";
 import DrawerHeader from "./DrawerHeader";
 
 export default function Drawer() {
@@ -6,6 +7,7 @@ export default function Drawer() {
       <div className="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-60"></div>
       <div className="fixed top-0 right-0 z-20 bg-white w-96 h-full p-8">
         <DrawerHeader />
+        <CartItemList />
       </div>
     </>
   );
