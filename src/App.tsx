ba20e9da751a92782941aa77b53659ac9c01846c
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 13:38:17 +0200

    Add container styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
       <Header />
       <Card />
-    </>
+    </div>
   );
 }
 
