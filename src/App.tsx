f29c6428ffba13f662fcb76d6c61ea500d061f51
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 14:38:28 +0200

    Use Card list component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Card from "./components/Card";
+import CardList from "./components/CardList";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
       <Header />
-      <Card />
+      <CardList />
     </div>
   );
 }
