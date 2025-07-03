ebc16c0a49600b118ee8fb99d7a3879747d91915
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 14:37:59 +0200

    Create properties inside Card component for future use

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,28 @@
 import SneakersLogo from "../assets/sneakers/sneakers-1.jpg";
 import PlusIcon from "../assets/plus.svg";
+import CheckedIcon from "../assets/checked.svg";
 import FavoriteIcon from "../assets/like-1.svg";
+import FavoriteAddedIcon from "../assets/like-2.svg";
 
-export default function Card() {
+interface CardProps {
+  title?: string;
+  imageUrl?: string;
+  price?: number;
+  isFavorite?: boolean;
+  isAdded?: boolean;
+}
+
+export default function Card({
+  title,
+  imageUrl,
+  price,
+  isFavorite = false,
+  isAdded = false,
+}: CardProps) {
   return (
     <div className="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
       <img
-        src={FavoriteIcon}
+        src={isFavorite ? FavoriteAddedIcon : FavoriteIcon}
         alt="Add to favorite icon"
         className="absolute top-8 left-8 cursor-pointer"
       />
@@ -18,7 +34,7 @@ export default function Card() {
           <p className="font-semibold">120 USD</p>
         </div>
         <img
-          src={PlusIcon}
+          src={!isAdded ? PlusIcon : CheckedIcon}
           alt="Add to a cart icon"
           className="cursor-pointer"
         />
