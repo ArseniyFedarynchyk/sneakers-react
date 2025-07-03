afc3d4c686908a22d7da41ad3a57e5d53d94bb9b
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 14:54:20 +0200

    Create Drawer

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,12 @@
+import DrawerHeader from "./DrawerHeader";
+
+export default function Drawer() {
+  return (
+    <>
+      <div className="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-60"></div>
+      <div className="fixed top-0 right-0 z-20 bg-white w-96 h-full p-8">
+        <DrawerHeader />
+      </div>
+    </>
+  );
+}
