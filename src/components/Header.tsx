b9796b344c88af13bbd9ac10ba911cfc26672b3a
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-03 12:38:06 +0200

    Create a header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import Logo from "../assets/logo.png";
+import CartIcon from "../assets/cart.svg";
+import ProfileIcon from "../assets/profile.svg";
+import FavoritesIcon from "../assets/heart.svg";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between border-b border-slate-200 px-10 py-4">
+      <div className="flex items-center gap-4">
+        <img src={Logo} alt="Logo" className="w-11" />
+        <div>
+          <h2 className="font-bold text-xl uppercase">React sneakers</h2>
+          <p className="text-slate-400">The best sneakers shop ever!</p>
+        </div>
+      </div>
+      <ul className="flex items-center gap-10">
+        <li className="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+          <img src={CartIcon} alt="Cart" />
+          <b>1205 USD</b>
+        </li>
+        <li className="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+          <img src={FavoritesIcon} alt="Favorite" />
+          <span>Favorites</span>
+        </li>
+        <li className="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+          <img src={ProfileIcon} alt="Profile" />
+          <span>Profile</span>
+        </li>
+      </ul>
+    </header>
+  );
+}
