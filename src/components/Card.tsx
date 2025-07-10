02e740395cd3e43ae7de5fed45f01bdd1e314c8b
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-10 20:21:46 +0200

    Map through images using imageUrl from backend

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,6 @@ import PlusIcon from "../assets/plus.svg";
 import CheckedIcon from "../assets/checked.svg";
 import FavoriteIcon from "../assets/like-1.svg";
 import FavoriteAddedIcon from "../assets/like-2.svg";
-import SneakerLogo from "../assets/sneakers/sneakers-1.jpg";
 import type { Sneaker } from "../models/sneaker.model";
 import { useContext } from "react";
 import { SneakerContext } from "../store/SneakerContext";
@@ -17,6 +16,45 @@ interface CardProps {
   isFavorites: boolean;
 }
 
+import sneaker1 from "../assets/sneakers/sneakers-1.jpg";
+import sneaker2 from "../assets/sneakers/sneakers-2.jpg";
+import sneaker3 from "../assets/sneakers/sneakers-3.jpg";
+import sneaker4 from "../assets/sneakers/sneakers-4.jpg";
+import sneaker5 from "../assets/sneakers/sneakers-5.jpg";
+import sneaker6 from "../assets/sneakers/sneakers-6.jpg";
+import sneaker7 from "../assets/sneakers/sneakers-7.jpg";
+import sneaker8 from "../assets/sneakers/sneakers-8.jpg";
+import sneaker9 from "../assets/sneakers/sneakers-9.jpg";
+import sneaker10 from "../assets/sneakers/sneakers-10.jpg";
+import sneaker11 from "../assets/sneakers/sneakers-11.jpg";
+import sneaker12 from "../assets/sneakers/sneakers-12.jpg";
+
+const sneakerImages = {
+  "sneakers-1": sneaker1,
+  "sneakers-2": sneaker2,
+  "sneakers-3": sneaker3,
+  "sneakers-4": sneaker4,
+  "sneakers-5": sneaker5,
+  "sneakers-6": sneaker6,
+  "sneakers-7": sneaker7,
+  "sneakers-8": sneaker8,
+  "sneakers-9": sneaker9,
+  "sneakers-10": sneaker10,
+  "sneakers-11": sneaker11,
+  "sneakers-12": sneaker12,
+};
+
+const getImageSource = (url?: string) => {
+  if (!url) return null;
+  const sneakerKey = url.split("/").pop();
+
+  if (sneakerKey && sneakerKey in sneakerImages) {
+    return sneakerImages[sneakerKey];
+  }
+
+  return null;
+};
+
 export default function Card({
   sneaker,
   title,
@@ -24,8 +62,10 @@ export default function Card({
   isFavorite,
   isAdded,
   isFavorites,
+  imageUrl,
 }: CardProps) {
   const { addToFavorites, onClickAddPlus } = useContext(SneakerContext);
+  const sneakerImage = getImageSource(imageUrl);
 
   return (
     <div className="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
@@ -37,7 +77,7 @@ export default function Card({
           onClick={() => addToFavorites(sneaker)}
         />
       )}
-      <img src={SneakerLogo} alt="sneaker photo" />
+      <img src={sneakerImage} alt="sneaker photo" />
       <p className="mb-5">{title}</p>
       <div className="flex justify-between">
         <div>
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,8 +1,46 @@
-import SneakersLogo from "../assets/sneakers/sneakers-1.jpg";
 import CloseIcon from "../assets/close.svg";
 import type { Sneaker } from "../models/sneaker.model";
 import { useContext } from "react";
 import { SneakerContext } from "../store/SneakerContext";
+
+import sneaker1 from "../assets/sneakers/sneakers-1.jpg";
+import sneaker2 from "../assets/sneakers/sneakers-2.jpg";
+import sneaker3 from "../assets/sneakers/sneakers-3.jpg";
+import sneaker4 from "../assets/sneakers/sneakers-4.jpg";
+import sneaker5 from "../assets/sneakers/sneakers-5.jpg";
+import sneaker6 from "../assets/sneakers/sneakers-6.jpg";
+import sneaker7 from "../assets/sneakers/sneakers-7.jpg";
+import sneaker8 from "../assets/sneakers/sneakers-8.jpg";
+import sneaker9 from "../assets/sneakers/sneakers-9.jpg";
+import sneaker10 from "../assets/sneakers/sneakers-10.jpg";
+import sneaker11 from "../assets/sneakers/sneakers-11.jpg";
+import sneaker12 from "../assets/sneakers/sneakers-12.jpg";
+
+const sneakerImages = {
+  "sneakers-1": sneaker1,
+  "sneakers-2": sneaker2,
+  "sneakers-3": sneaker3,
+  "sneakers-4": sneaker4,
+  "sneakers-5": sneaker5,
+  "sneakers-6": sneaker6,
+  "sneakers-7": sneaker7,
+  "sneakers-8": sneaker8,
+  "sneakers-9": sneaker9,
+  "sneakers-10": sneaker10,
+  "sneakers-11": sneaker11,
+  "sneakers-12": sneaker12,
+};
+
+const getImageSource = (url?: string) => {
+  if (!url) return null;
+  const sneakerKey = url.split("/").pop();
+
+  if (sneakerKey && sneakerKey in sneakerImages) {
+    return sneakerImages[sneakerKey];
+  }
+
+  return null;
+};
 
 interface CartItemProps {
   cartItem: Sneaker;
@@ -10,10 +48,11 @@ interface CartItemProps {
 
 export default function CartItem({ cartItem }: CartItemProps) {
   const { removeFromCart } = useContext(SneakerContext);
+  const sneakerLogo = getImageSource(cartItem.imageUrl);
 
   return (
     <div className="flex items-center gap-4 w-full border border-slate-200 p-4 rounded-xl">
-      <img className="w-16" src={SneakersLogo} alt="Sneaker logo" />
+      <img className="w-16" src={sneakerLogo} alt="Sneaker logo" />
       <div className="flex flex-col flex-1">
         <p>{cartItem.title}</p>
         <div className="flex justify-between mt-2">
