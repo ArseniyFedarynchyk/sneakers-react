060522268bf7589b6f751e97fabbcd7a4c06a764
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-12 13:15:39 +0200

    Make root layout respect RWD

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
   return (
     <>
       {isCartOpen && <Drawer />}
-      <div className="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+      <div className="bg-white md:w-4/5 md:m-auto md:rounded-xl md:shadow-xl md:mt-14">
         <Header />
         <div className="p-10">
           <Outlet />
