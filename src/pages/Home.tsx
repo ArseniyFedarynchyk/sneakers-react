e208273485018d981c0b1ac775e64bc2a1996d52
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-12 13:13:37 +0200

    Make home page align with RWD

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,10 +9,12 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex justify-between items-center mb-8">
-        <h2 className="text-3xl font-bold">All sneakers</h2>
+      <div className="flex flex-col md:flex-row justify-between items-center mb-8">
+        <h2 className="text-2xl md:text-3xl font-bold mb-4 md:mb-0">
+          All sneakers
+        </h2>
 
-        <div className="flex gap-3">
+        <div className="flex flex-col md:flex-row gap-3 w-full md:w-auto">
           <select
             className="py-2 px-3 border border-gray-200 rounded outline-none"
             name="sort"
@@ -24,10 +26,10 @@ export default function Home() {
             <option value="-price">By price (high - low)</option>
           </select>
 
-          <div className="relative">
+          <div className="relative w-full md:w-auto">
             <img className="absolute left-3 top-3" src={SearchIcon} alt="" />
             <input
-              className="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              className="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400 w-full"
               type="text"
               placeholder="Search..."
               value={filters.searchQuerry}
