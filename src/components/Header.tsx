a20ab6c038921a4c891d2daf846c08140f6f6173
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-15 13:48:15 +0200

    Make header align with RWD

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,33 +10,35 @@ export default function Header() {
   const { totalPrice, toggleCart } = useContext(SneakerContext);
 
   return (
-    <header className="sticky top-0 z-5 bg-white flex justify-between border-b border-slate-200 px-10 py-4">
+    <header className="sticky top-0 z-5 bg-white flex justify-between border-b border-slate-200 px-5 sm:px-10 py-4">
       <Link to="/">
         <div className="flex items-center gap-4">
           <img src={Logo} alt="Logo" className="w-11" />
           <div>
-            <h2 className="font-bold text-xl uppercase">React sneakers</h2>
-            <p className="text-slate-400">The best sneakers shop ever!</p>
+            <h2 className="font-bold md:text-xl uppercase">React sneakers</h2>
+            <p className="hidden sm:block text-slate-400">
+              The best sneakers shop ever!
+            </p>
           </div>
         </div>
       </Link>
-      <ul className="flex items-center gap-10">
+      <ul className="flex items-center gap-5">
         <li
           className="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black"
           onClick={toggleCart}
         >
           <img src={CartIcon} alt="Cart" />
-          <b>{totalPrice} USD</b>
+          <b className="hidden md:block">{totalPrice} USD</b>
         </li>
         <Link to="/favorites">
           <li className="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
             <img src={FavoritesIcon} alt="Favorite" />
-            <span>Favorites</span>
+            <span className="hidden md:block">Favorites</span>
           </li>
         </Link>
         <li className="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
           <img src={ProfileIcon} alt="Profile" />
-          <span>Profile</span>
+          <span className="hidden md:block">Profile</span>
         </li>
       </ul>
     </header>
