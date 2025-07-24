9fb57afbd24a7bee9ce6989c757661e933d9a894
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-24 16:11:09 +0200

    Navigate to a different route when a card was clicked and stop propagation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,7 @@ import FavoriteAddedIcon from "../assets/like-2.svg";
 import type { Sneaker } from "../models/sneaker.model";
 import { useContext } from "react";
 import { SneakerContext } from "../store/SneakerContext";
+import { useNavigate } from "react-router";
 import { getImageSource } from "../helper";
 
 interface CardProps {
@@ -17,45 +18,6 @@ interface CardProps {
   isFavorites: boolean;
 }
 
-import sneaker1 from "../assets/sneakers/sneakers-1.jpg";
-import sneaker2 from "../assets/sneakers/sneakers-2.jpg";
-import sneaker3 from "../assets/sneakers/sneakers-3.jpg";
-import sneaker4 from "../assets/sneakers/sneakers-4.jpg";
-import sneaker5 from "../assets/sneakers/sneakers-5.jpg";
-import sneaker6 from "../assets/sneakers/sneakers-6.jpg";
-import sneaker7 from "../assets/sneakers/sneakers-7.jpg";
-import sneaker8 from "../assets/sneakers/sneakers-8.jpg";
-import sneaker9 from "../assets/sneakers/sneakers-9.jpg";
-import sneaker10 from "../assets/sneakers/sneakers-10.jpg";
-import sneaker11 from "../assets/sneakers/sneakers-11.jpg";
-import sneaker12 from "../assets/sneakers/sneakers-12.jpg";
-
-const sneakerImages = {
-  "sneakers-1": sneaker1,
-  "sneakers-2": sneaker2,
-  "sneakers-3": sneaker3,
-  "sneakers-4": sneaker4,
-  "sneakers-5": sneaker5,
-  "sneakers-6": sneaker6,
-  "sneakers-7": sneaker7,
-  "sneakers-8": sneaker8,
-  "sneakers-9": sneaker9,
-  "sneakers-10": sneaker10,
-  "sneakers-11": sneaker11,
-  "sneakers-12": sneaker12,
-};
-
-const getImageSource = (url?: string) => {
-  if (!url) return null;
-  const sneakerKey = url.split("/").pop();
-
-  if (sneakerKey && sneakerKey in sneakerImages) {
-    return sneakerImages[sneakerKey];
-  }
-
-  return null;
-};
-
 export default function Card({
   sneaker,
   title,
@@ -67,15 +29,26 @@ export default function Card({
 }: CardProps) {
   const { addToFavorites, onClickAddPlus } = useContext(SneakerContext);
   const sneakerImage = getImageSource(imageUrl);
+  const navigate = useNavigate();
+
+  function redirectToDetailsPage() {
+    navigate("/sneakers/" + sneaker.id);
+  }
 
   return (
-    <div className="relative flex flex-col justify-between bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
+    <div
+      className="relative flex flex-col justify-between bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+      onClick={redirectToDetailsPage}
+    >
       {!isFavorites && (
         <img
           src={isFavorite ? FavoriteAddedIcon : FavoriteIcon}
           alt="Add to favorite icon"
           className="absolute top-8 left-8 cursor-pointer"
-          onClick={() => addToFavorites(sneaker)}
+          onClick={(e) => {
+            e.stopPropagation();
+            addToFavorites(sneaker);
+          }}
         />
       )}
       <img src={sneakerImage} alt="sneaker photo" />
@@ -92,7 +65,10 @@ export default function Card({
             className="cursor-pointer"
             width="30"
             height="30"
-            onClick={() => onClickAddPlus(sneaker)}
+            onClick={(e) => {
+              e.stopPropagation();
+              onClickAddPlus(sneaker);
+            }}
           />
         )}
       </div>
