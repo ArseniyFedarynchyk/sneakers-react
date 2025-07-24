c4d32328c2e5f86e9e440298dd92549ceaa98777
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-07-24 16:08:47 +0200

    Change naming of different route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
+import HomePage from "./pages/HomePage";
 import SneakerProvider from "./store/SneakerContext";
 import RootLayout from "./layouts/RootLayout";
 import SneakerDetailsPage, {
   loader as sneakerDetailsLoader,
 } from "./pages/SneakerDetailsPage";
+import FavoritesPage from "./pages/FavoritesPage";
+
+export const API_URL = "https://f67e77c455aa171b.mokky.dev";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
+      { index: true, element: <HomePage /> },
+      { path: "favorites", element: <FavoritesPage /> },
       {
         path: "sneakers/:sneakerId",
         element: <SneakerDetailsPage />,
