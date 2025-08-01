e04d97739db83c54bd549a196ad5aad0a5b55374
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-08-01 15:43:15 +0200

    Create a reusable Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+export default function Input(props) {
+  return (
+    <input
+      className="border border-gray-200 rounded-md py-2 px-4 outline-none focus:border-gray-400 w-full"
+      {...props}
+    />
+  );
+}
