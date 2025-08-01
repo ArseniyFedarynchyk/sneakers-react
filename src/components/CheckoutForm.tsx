35b96a053b41d3c1251b5b5eabc5e8439ab70d90
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-08-01 15:43:23 +0200

    Create checkout form

---
+++ src/components/CheckoutForm.tsx
@@ -0,0 +1,41 @@
+import Input from "./Input";
+
+export default function CheckoutForm() {
+  return (
+    <form className="flex flex-col gap-3">
+      <Input type="text" placeholder="First Name" />
+      <Input type="text" placeholder="Last Name" />
+      <Input type="email" placeholder="Email" />
+      <div className="flex gap-3">
+        <Input type="text" placeholder="Street" />
+        <Input
+          className="basis-1/5 border border-gray-200 rounded-md py-2 px-4 outline-none focus:border-gray-400 w-full"
+          type="text"
+          placeholder="No."
+        />
+        <Input
+          className="basis-1/5 border border-gray-200 rounded-md py-2 px-4 outline-none focus:border-gray-400 w-full"
+          type="text"
+          placeholder="Apt No."
+        />
+      </div>
+      <div className="flex gap-3">
+        <Input
+          className="basis-1/5 border border-gray-200 rounded-md py-2 px-4 outline-none focus:border-gray-400 w-full"
+          type="text"
+          placeholder="Postal Code"
+        />
+        <Input type="text" placeholder="City" />
+      </div>
+      <Input type="tel" placeholder="Phone number" />
+      <div className="flex justify-between">
+        <button className="transition bg-slate-400 rounded-xl cursor-pointer p-3 text-white hover:bg-slate-500">
+          Cancel
+        </button>
+        <button className="transition bg-lime-500 rounded-xl cursor-pointer p-3 text-white hover:bg-lime-600">
+          Checkout
+        </button>
+      </div>
+    </form>
+  );
+}
