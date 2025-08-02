db52cc6301a834a8055daad55c503ba5c2f5adb1
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-08-02 09:23:34 +0200

    Change shipping details model to accept only strings

---
+++ src/models/shippingDetails.model.ts
@@ -0,0 +1,11 @@
+export interface ShippingDetails {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  street: string;
+  streetNumber: string;
+  apartment: string;
+  postalCode: string;
+  city: string;
+}
