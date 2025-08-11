82b375c80094aa1bf27242e3e577b2625044b9cf
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-08-11 14:03:33 +0200

    Add props to reusable input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,11 @@
-export default function Input(props) {
-  return (
-    <input
-      className="border border-gray-200 rounded-md py-2 px-4 outline-none focus:border-gray-400 w-full"
-      {...props}
-    />
-  );
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  isValid: boolean;
+}
+
+export default function Input({ isValid, ...props }: Props) {
+  const classes = isValid
+    ? "border border-red-300 rounded-md py-2 px-4 outline-none focus:border-red-400 w-full"
+    : "border border-gray-300 rounded-md py-2 px-4 outline-none focus:border-gray-400 w-full";
+
+  return <input className={classes} {...props} />;
 }
