4973a80adcbbf5ea31ad8ff9bfe22b40a3b997c8
Arseniy Fedarynchyk <1995arseny@gmail.com>
2025-08-16 10:50:24 +0200

    Create loading spinner and use it

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full border-8 border-t-8 border-gray-200 border-t-blue-500 w-16 h-16"></div>
+    </div>
+  );
+}
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,16 +3,19 @@ import Header from "../components/Header";
 import { SneakerContext } from "../store/SneakerContext";
 import Drawer from "../components/Drawer";
 import { Outlet } from "react-router";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function RootLayout() {
-  const { error, isCartOpen } = useContext(SneakerContext);
+  const { error, isLoading, isCartOpen } = useContext(SneakerContext);
 
   return (
     <>
       {isCartOpen && <Drawer />}
       <div className="bg-white md:w-4/5 md:m-auto md:rounded-xl md:shadow-xl md:mt-14">
         <Header />
-        <div className="p-10">{error ? error : <Outlet />}</div>
+        <div className="p-10">
+          {isLoading ? <LoadingSpinner /> : error ? error : <Outlet />}
+        </div>
       </div>
     </>
   );
